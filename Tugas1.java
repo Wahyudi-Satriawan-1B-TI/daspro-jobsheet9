dbac9b4b9d0b7f030f9f2531450b9236192c3c04
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2024-10-30 11:23:01 +0700

    Penyelesaian Tugas 1

---
+++ Tugas1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    int w;
+    double rata2, total = 0;
+    
+    System.out.println("========================================================");
+    System.out.print("Masukkan jumlah mahasiswa: ");
+    w = sc.nextInt();
+    
+    int[] nilaiMhs = new int[w];
+
+    System.out.println("========================================================");
+    for (int i = 0; i < nilaiMhs.length; i++) {
+        System.out.print("Masukkan Nilai Mahasiswa ke-" + (i+1) + " : ");
+        nilaiMhs [i] = sc.nextInt();
+    }
+    for (int i = 0; i < nilaiMhs.length; i++) {
+        total += nilaiMhs[i];
+    }
+
+    rata2 = total/nilaiMhs.length;
+
+    System.out.println("========================================================");
+    System.out.println("Rata-rata nilai = " + rata2);
+    System.out.println("========================================================");
+    sc.close();
+
+    int nilaiTertinggi = nilaiMhs[0];
+    int nilaiTerendah = nilaiMhs[0];
+
+    for (int i = 1; i < nilaiMhs.length; i++){
+        if (nilaiMhs[i] > nilaiTertinggi){
+            nilaiTertinggi = nilaiMhs[i];
+        }
+        if (nilaiMhs[i] < nilaiTerendah){
+            nilaiTerendah = nilaiMhs[i];
+        }
+    }
+    System.out.println("Nilai Tertinggi: " + nilaiTertinggi);
+    System.out.println("Nilai Terendah: " + nilaiTerendah);
+    System.out.println("========================================================");
+
+    for (int i = 0; i < nilaiMhs.length; i++){
+        System.out.println("Mahasiswa ke-" +(i+1)+ " Mendapatkan nilai " +nilaiMhs [i]);
+    }
+    System.out.println("========================================================");
+    }
+}
+/*
+- memasukkan banyaknya nilai mahasiswa yang akan diinput,
+- memasukkan setiap nilai mahasiswa,
+- menghitung nilai rata-rata,
+- menampilkan nilai tertinggi dan nilai terendah, serta
+- menampilkan semua nilai yang telah dimasukkan.*/
